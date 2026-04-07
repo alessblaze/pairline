3746e289ee40ff390ebb214e1bafe1fc1e57e3b3
Albert Alesso <anishsheikh2000@icloud.com>
2026-04-07 23:04:56 +0530

    added headers.

--- backend/elixir/omegle_phoenix/start.sh
+++ backend/elixir/omegle_phoenix/start.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Pairline - Open Source Video Chat and Matchmaking
+# Copyright (C) 2026 Albert Blasczykowski
+# Aless Microsystems
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 # Load environment variables from .env file if it exists and isn't explicitly disabled.
 # Existing environment variables win, which keeps Docker/provisioned env stable.
